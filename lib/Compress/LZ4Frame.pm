455ea2cfc2e18dfcd0cf3631dc34eff73f7bb3ed
Felix Bytow <felix.bytow@autinity.de>
2017-02-24 12:20:19 +0100

    Build results of a538793 (on master)

--- lib/Compress/LZ4Frame.pm
+++ lib/Compress/LZ4Frame.pm
@@ -1,6 +1,6 @@
 package Compress::LZ4Frame;
 # ABSTRACT: Compression package using the lz4frame library
-$Compress::LZ4Frame::VERSION = '0.011001';
+$Compress::LZ4Frame::VERSION = '0.012';
 use 5.010_001;
 use strict;
 use warnings;
@@ -28,7 +28,7 @@ Compress::LZ4Frame - Compression package using the lz4frame library
 
 =head1 VERSION
 
-version 0.011001
+version 0.012
 
 =head1 SYNOPSIS
 
